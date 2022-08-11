dc8e10ec9f41e5338ff706c753fc4b206279d49b
Jon Boland <jon.boland@gmail.com>
2022-08-11 10:18:29 +0100

    Tweak first test

--- tests/test_workingdays.py
+++ tests/test_workingdays.py
@@ -7,7 +7,7 @@ from workingdays import calculate_working_days
 def test_calculate_working_days():
     start_day = 3
     month_length = 31
-    total_days = month_length - (start_day - 1)
+    total_days = month_length - (start_day)
     weekend_days = 8
     bank_holidays = 2
     expected = total_days - weekend_days - bank_holidays
