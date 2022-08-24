c1a0c87b8acbf1b815701887bfa4e0be90148e15
Jon Boland <jon.boland@gmail.com>
2022-08-24 15:42:32 +0100

    Add __init__ and remove context

--- tests/context.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import sys
-
-# Enables workingdays module imports when running tests
-context = Path(__file__).resolve().parents[1] / "workingdays"
-sys.path.insert(0, str(context))
